45e2ff94ecfe9584ff67343f0d1a5ace5ffcd591
colorzzr <colorzzr@gmail.com>
2020-10-10 14:34:30 -0400

    archieve the template page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -15,7 +15,7 @@ export default () => (
   <PageContainer>
     <Card>
       <Alert
-        message="更快更强的重型组件，已经发布。"
+        message="ECE444 Group3 Quaranteam"
         type="success"
         showIcon
         banner
@@ -25,24 +25,8 @@ export default () => (
         }}
       />
       <Typography.Text strong>
-        高级表格{' '}
-        <a href="https://protable.ant.design/" rel="noopener noreferrer" target="__blank">
-          欢迎使用
-        </a>
+        Main Page
       </Typography.Text>
-      <CodePreview>yarn add @ant-design/pro-table</CodePreview>
-      <Typography.Text
-        strong
-        style={{
-          marginBottom: 12,
-        }}
-      >
-        高级布局{' '}
-        <a href="https://prolayout.ant.design/" rel="noopener noreferrer" target="__blank">
-          欢迎使用
-        </a>
-      </Typography.Text>
-      <CodePreview>yarn add @ant-design/pro-layout</CodePreview>
     </Card>
   </PageContainer>
 );
