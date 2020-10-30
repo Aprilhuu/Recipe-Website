66ec34166cef6b300fe01f59edf012bf17f7c5b0
colorzzr <colorzzr@gmail.com>
2020-10-29 21:28:52 -0400

    update gunicorn file

--- flask-api/gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-# gunicorn_config.py
-workers = 4
-threads = 2
-bind = '0.0.0.0:5000'
-daemon = 'false'
-worker_connections = 5
-# accesslog = 'access.log'
-# errorlog = 'error.log'
-# loglevel = 'info'
