08a39de279fd9a996ea612cdb9d8b411251fd697
colorzzr <colorzzr@gmail.com>
2020-10-29 21:37:28 -0400

    switch to not use gunicorn

--- flask-api/app.py
+++ flask-api/app.py
@@ -3,5 +3,10 @@ from app import create_app
 
 app = create_app()
 
+@app.route("/test") 
+def home_view(): 
+        return "<h1>Welcome to Geeks for Geeks</h1>"
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
