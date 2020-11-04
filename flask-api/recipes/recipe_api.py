39f27022171881ba4c4ce948cbf0df0292189802
Aprilhuu <38861292+Aprilhuu@users.noreply.github.com>
2020-11-04 14:31:27 -0500

    Feature: (CCP-59, CCP-26) Adding backend support to search by ingredients and search by titles

--- flask-api/recipes/recipe_api.py
+++ flask-api/recipes/recipe_api.py
@@ -1,12 +1,13 @@
 from flask_restx import Resource
 from flask import request
 from bson.objectid import ObjectId
+import inflect
 
 from app import db_connection
 
 # this api instance is to handle with all the recipes
 class Recipes(Resource):
-    
+
     def get(self):
         '''
         Retrieve all the recipe id and title for list displaying
@@ -22,13 +23,13 @@ class Recipes(Resource):
         except Exception as e:
             return {'result': str(e)}, 400
 
-        return {'result':recipes}, 200
+        return {'result': recipes}, 200
 
 
 # this api instance deal with the specific recipe
 # the recipe is target by rid
 class Recipe(Resource):
-    
+
     def get(self, rid):
         '''
         Retrieve the recipe detail by id
@@ -47,36 +48,73 @@ class Recipe(Resource):
         except Exception as e:
             return {'result': str(e)}, 400
 
-        return {'result':recipe}, 200
-
+        return {'result': recipe}, 200
 
 
 class RecipeQuery(Resource):
-    
+
     def post(self):
         '''
-        Retrieve the recipe detail by input attribute
-        for now it is only query by title
+        Retrieve the recipe detail by ingredient attributes and/or recipe title
         '''
 
-        # example curl -v -XPOST -H "Content-type: application/json" -d '{"title":"Fruit and Nut Oat Bowl"}' 'localhost:5000/v1/recipes/query'
+        # Example:
+
+        # curl -v -XPOST -H "Content-type: application/json" -d '{"title":"Fruit and Nut Oat Bowl",
+        # "ingredients": ["beef", "apple"]}' 'localhost:5000/v1/recipes/query'
 
         try:
-            # first get the title from the payload
+            # Step 1: Get user query input from the payload (i.e. ingredients or title)
             post_data = request.get_json()
             title = post_data.get('title', None)
-            if not title:
-                return {'result':'Please input the title'}, 403
+            ingredients = post_data.get('ingredients', None)
+            if not title and not ingredients:
+                return {'result': 'Please input search title or search ingredients'}, 403
 
+            # Step 2: Prepare MongoDB query filter object based on user input
+            ingredient_filter_array = []
+            if ingredients:
+                p = inflect.engine()
+                for ingredient in ingredients:
+                    # We are considering both plural and singular form of ingredients here
+                    if not ingredient.endswith('s'):
+                        plural_form = p.plural_noun(ingredient)
+                        singular_form = ingredient
+                    else:
+                        singular_form = p.singular_noun(ingredient)
+                        plural_form = ingredient
+                    # Using regex to perform contain as substring instead of exactly matching
+                    ingredient_filter_array.append({'$or':
+                                                      [{'ingredients.name': {'$regex': '.*' + singular_form + '.*'}},
+                                                       {'ingredients.name': {'$regex': '.*' + plural_form + '.*'}}]})
+
+            # Step 3: Query the database collection based on preprocessed filter
             collection = db_connection["group3_collection"]
-            # for now just return the 8 recipe in total
-            # to keep minimun only return the id and title of list
-            recipe = collection.find_one({'title': title})
+            if title and ingredients:
+                # Adding title as one filter as well to filter both by title and by ingredients
+                ingredient_filter_array.append({'title': title})
+                recipe = collection.find({'$and': ingredient_filter_array})
+            elif ingredients:
+                recipe = collection.find({'$and': ingredient_filter_array})
+            else:
+                # TODO: Right now only support search by exact title. Do we need to support more
+                #  flexible search later?
+                recipe = collection.find_one({'title': title})
 
-            # change the id to string if we find it
-            if recipe:
+            # Step 4: Process results returned from database before returning. We are
+            # changing the id to string if we find it and remove unnecessary attributes.
+            if isinstance(recipe, dict):
                 recipe['_id'] = str(recipe['_id'])
+                del recipe['ingredients']
+                del recipe['instructions']
+            else:
+                recipe = list(recipe)
+                for one_recipe in recipe:
+                    one_recipe['_id'] = str(one_recipe['_id'])
+                    del one_recipe['ingredients']
+                    del one_recipe['instructions']
+
         except Exception as e:
             return {'result': str(e)}, 400
 
-        return {'result':recipe}, 200
+        return {'result': recipe}, 200
--- flask-api/test/recipe_api_test.py
+++ flask-api/test/recipe_api_test.py
@@ -33,10 +33,26 @@ def get_recipe_by_title(client, title):
         follow_redirects=True,
     )
 
+# made by ZIAN HU
+def get_recipe_by_ingredients(client, ingredients):
+    return client.post(
+        "/v1/recipes/query",
+        json={'ingredients': ingredients},
+        follow_redirects=True,
+    )
+
+# made by ZIAN HU
+def get_recipe_by_ingredients_and_title(client, ingredients, title):
+    return client.post(
+        "/v1/recipes/query",
+        json={'ingredients': ingredients, 'title': title},
+        follow_redirects=True,
+    )
+
+
 ################################################ TEST FUnction ######################################
 # made by justin
 def test_get_recipe_by_id(client):
-
     # check if Tofu Breakfast Burrito recipe title and id is returned
     res = get_recipe_by_id(client, '5f8c67b8708d83b9867302b6')
     recipe_res = res.get_json()['result']
@@ -55,6 +71,7 @@ def test_get_recipe_by_title(client):
         'id': str(recipe_res['_id']), 'title': recipe_res['title']}
     assert recipe_title_id == {
         'id': '5f8c67b8708d83b9867302b6', 'title': "Tofu Breakfast Burrito"}
+    assert 'instructions' not in recipe_res.keys()
 
     # check if we can find recipe "Creamy Sweet Chili Shrimp"
     res = get_recipe_by_title(client, "Creamy Sweet Chili Shrimp")
@@ -63,4 +80,75 @@ def test_get_recipe_by_title(client):
         'id': str(recipe_res['_id']), 'title': recipe_res['title']}
     assert recipe_title_id == {
         'id': '5f8c67b7708d83b9867302af', 'title': "Creamy Sweet Chili Shrimp"}
+    assert 'ingredients' not in recipe_res.keys()
+
+
+# made by ZIAN HU
+def test_get_recipe_by_ingredients(client):
+    # Test 1: Basic testing with two ingredients (singular form)
+    # check if we can find recipe containing brown sugar and apple
+    res = get_recipe_by_ingredients(client, ["brown sugar", "apple"])
+    recipe_res = res.get_json()['result']
+    recipe_title_id = {
+        'id': str(recipe_res[0]['_id']), 'title': recipe_res[0]['title']}
+    assert recipe_title_id == {
+        'id': '5f8c67b8708d83b9867302b1', 'title': "Baked Apples"}
+
+    # Test 2: Basic testing with one ingredient (singular form) and multiple results
+    # check if we can find recipe containing oat. The original recipes
+    # specify quick cooking oats and steel cut oats. This should not
+    # affect the result
+    res = get_recipe_by_ingredients(client, ["oat"])
+    recipe_res = res.get_json()['result']
+    assert len(recipe_res) >= 2
+    recipe_title_id = {
+      'id': str(recipe_res[0]['_id']), 'title': recipe_res[0]['title']}
+    assert recipe_title_id == {
+      'id': '5f8c67b8708d83b9867302b1', 'title': "Baked Apples"}
+    recipe_title_id = {
+      'id': str(recipe_res[1]['_id']), 'title': recipe_res[1]['title']}
+    assert recipe_title_id == {
+      'id': '5f8c67b8708d83b9867302b5', 'title': "Fruit and Nut Oat Bowl"}
+    assert 'ingredients' not in recipe_res[1].keys() and "instructions" not in recipe_res[1].keys()
+
+    # Test 3: Basic testing with no applicable recipe
+    # check if we can find recipe containing "weird ingredient".
+    # No recipe should contain this ingredient
+    res = get_recipe_by_ingredients(client, ["weird ingredient"])
+    recipe_res = res.get_json()['result']
+    assert len(recipe_res) == 0
+
+    # Test 4: Basic testing with different noun plural form
+    # check if we can find recipe containing "berry".
+    # Recipes with both "berries" and "berry" as ingredients should be returned
+    res = get_recipe_by_ingredients(client, ["berry"])
+    recipe_res = res.get_json()['result']
+    assert len(recipe_res) >= 1
+    recipe_title_id = {
+      'id': str(recipe_res[0]['_id']), 'title': recipe_res[0]['title']}
+    assert recipe_title_id == {
+      'id': '5f8c67b8708d83b9867302b5', 'title': "Fruit and Nut Oat Bowl"}
+
+
+# made by ZIAN HU
+def test_get_recipe_by_ingredients_and_title(client):
+    # Test 1: Basic testing with two ingredients (singular form) and title
+    # check if we can find recipe containing brown sugar and apple
+    res = get_recipe_by_ingredients_and_title(client, ["brown sugar", "apple"], "Baked Apples")
+    recipe_res = res.get_json()['result']
+    recipe_title_id = {
+        'id': str(recipe_res[0]['_id']), 'title': recipe_res[0]['title']}
+    assert recipe_title_id == {
+        'id': '5f8c67b8708d83b9867302b1', 'title': "Baked Apples"}
+
+    # Test 2: Basic testing with one ingredient (singular form) and multiple results
+    # The title condition should restrict results down to one
+    res = get_recipe_by_ingredients_and_title(client, ["oat"], "Fruit and Nut Oat Bowl")
+    recipe_res = res.get_json()['result']
+    assert len(recipe_res) == 1
+    recipe_title_id = {
+      'id': str(recipe_res[0]['_id']), 'title': recipe_res[0]['title']}
+    assert recipe_title_id == {
+      'id': '5f8c67b8708d83b9867302b5', 'title': "Fruit and Nut Oat Bowl"}
+    assert 'ingredients' not in recipe_res[0].keys() and "instructions" not in recipe_res[0].keys()
 
