992c95623997858e000406c86290f493d95ea63b
Bella <isabella.chen@mail.utoronto.ca>
2020-11-06 19:38:58 -0800

    feature/CCP-69: implment allowance of removing items from ticked list add notifications

--- src/pages/ShoppingList.jsx
+++ src/pages/ShoppingList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Card, List, Popover, Checkbox, PageHeader, Button } from 'antd';
+import { Card, List, Popover, Checkbox, PageHeader, notification } from 'antd';
 import axios from 'axios';
+import { CheckOutlined, PlusOutlined, SmileOutlined } from '@ant-design/icons';
 
 import defaultSettings from '../../config/defaultSettings';
 const { api_endpoint } = defaultSettings
@@ -18,57 +19,113 @@ class ShoppingList extends Component {
         this.onChange = this.onChange.bind(this);
     }
 
-    onChange(e) {
-        // get the id
-        console.log(this)
-        console.log(e)
-
-        let entry_id = e.target.id.split("-")[0]
-        
-        // move to ticked list
-        let { tickedListInfo, listInfo } = this.state;
-        // console.log(listInfo);
-        tickedListInfo[entry_id] = JSON.parse(JSON.stringify(listInfo[entry_id]))
-        delete listInfo[entry_id]
-        this.setState({ tickedListInfo: tickedListInfo, listInfo: listInfo })
-
-        this.updateList(listInfo, false)
-        this.updateList(tickedListInfo, true)
+    openNotification = (title, description) => {
+        notification.open({
+            message: title,
+            description: description,
+            duration: 8,
+            icon: <SmileOutlined style={{ color: '#108ee9' }} />,
+        });
     }
 
-    // how do we want to save the shopping list?
-    printStatus() {
-        for (let i = 0; i < document.getElementsByClassName("ant-list-item").length; i++) {
-            console.log(document.getElementsByClassName("ant-list-item")[i].childNodes)
+    onChange(e) {
+
+        // console.log(this)
+        // console.log(e)
+
+        // get the id
+        let parsed_id = e.target.id.split("-")
+        let entry_id = parsed_id[0]
+        let checkboxType = parsed_id[1]
+    
+        let { tickedListInfo, listInfo } = this.state;
+        if (checkboxType == 'tickbox') {
+            this.openNotification('1 item ticked', 'If this was a misclick, click the \'+\' button in your Ticked Items list below to add the item back.')
+
+            // move to ticked list
+            tickedListInfo[entry_id] = JSON.parse(JSON.stringify(listInfo[entry_id]))
+            delete listInfo[entry_id]
+            this.setState({ tickedListInfo: tickedListInfo, listInfo: listInfo })
+        } else {
+            // move back to checked list
+            this.openNotification('1 item unticked', '')
+
+            listInfo[entry_id] = JSON.parse(JSON.stringify(tickedListInfo[entry_id]))
+            delete tickedListInfo[entry_id]
+            this.setState({ tickedListInfo: tickedListInfo, listInfo: listInfo })
         }
+
+        //update shopping list
+        this.updateList(listInfo, false)
         
+        //update ticked list
+        this.updateList(tickedListInfo, true)
     }
 
     updateList(list, tickedItems) {
         let items = [];
+        let { curId } = this.state;
+        let listInfo = null;
+
+        // curId == 0 only if the shopping list is empty
+        // and we're going to populate it for the first time
+        // existance of listInfo is used to check for first time later on
+        if (curId == 0) {
+            listInfo = {};
+        }
+
 
         for (const [key, value] of Object.entries(list)) {
             let popoverItem = [];
 
-            for (const [key, value] of Object.entries(value.detail)) {
+            // initial update
+            if (typeof listInfo != 'undefined' && listInfo != null) {
+                listInfo[curId] = {};
+                listInfo[curId].quantity = value.quantity;
+                listInfo[curId].detail = value.detail;
+                listInfo[curId].title = key
+            }
 
+            let i = 0;
+            for (const [key, value] of Object.entries(value.detail)) {
+            
                 // popup that shows after hovering each shopping list item
                 popoverItem.push(
-                    <div key={value.recipe_id}>
+                    <div key={i}>
                         <a href={"/recipe/" + value.recipe_id}>{value.recipe_title}</a> needs {value.quantity} of this ingredient
                     </div>
                 )
+            i++;
             }
 
-            const popoverKey = value.title + '-popover'
             const checkbox = []
-            if (!tickedItems) checkbox = <Checkbox id={key + '-checkbox'} onChange={this.onChange}></Checkbox>
-            
-            // each shopping list item
+            let checkboxId = key;
+            let titleKey = value.title;
+
+            // initial update
+            if (typeof listInfo != 'undefined' && listInfo != null) {
+                checkboxId = curId;
+                titleKey = key
+            }
+
+            let checkSign;
+            if (tickedItems) {
+                checkboxId += '-addbackbox';
+                checkSign = <PlusOutlined />
+            } else {
+                checkboxId += '-tickbox';
+                checkSign = <CheckOutlined/>
+            }
+                
+            checkbox = <Checkbox id={checkboxId} onChange={this.onChange}>
+                    {checkSign}
+                </Checkbox>
+
+            const popoverKey = titleKey + '-popover'
             items.push(
-                <List.Item key={value.title}>
+                <List.Item key={titleKey}>
                     <List.Item.Meta 
-                        title={value.title}
+                        title={titleKey}
                         description={
                         <Popover key={popoverKey} content={popoverItem} title="Recipe and Amount Needed">
                             <div>Needed amount for all recipes: {value.quantity}</div>
@@ -78,8 +135,14 @@ class ShoppingList extends Component {
                     {checkbox}
                 </List.Item>
             )
-
-            console.log(items)
+            
+            // initial update
+            if (typeof listInfo != 'undefined' && listInfo != null) curId++;
+        }
+        // initial update
+        if (typeof listInfo != 'undefined' && listInfo != null) {
+            this.setState({ listItem: items, curId: curId, listInfo: listInfo})
+        } else {
             if (tickedItems) this.setState({ tickedListItem: items })
             else this.setState({ listItem: items })
         }
@@ -89,78 +152,32 @@ class ShoppingList extends Component {
     componentDidMount(){
         axios.get(api_endpoint+'/v1/users/meal_plan/shopping_list', {}).then(
             response => { 
-                let listItem = [];
-                let listInfo = {};
-                let { curId } = this.state;
-
-                // get the response and parse
-                for (const [key, value] of Object.entries(response.data.result)) {
-                    let popoverItem = [];
-                
-                    listInfo[curId] = {};
-                    listInfo[curId].quantity = value.quantity;
-                    listInfo[curId].detail = value.detail;
-                    listInfo[curId].title = key
-
-                    for (const [key, value] of Object.entries(value.detail)) {
-
-                        // popup that shows after hovering each shopping list item
-                        popoverItem.push(
-                            <div key={value.recipe_id}>
-                                <a href={"/recipe/" + value.recipe_id}>{value.recipe_title}</a> needs {value.quantity} of this ingredient
-                            </div>
-                        )
-                    }
-
-                    const popoverKey = key + '-popover'
-                    
-                    // each shopping list item
-                    listItem.push(
-                        <List.Item key={key}>
-                            <List.Item.Meta 
-                                title={key}
-                                description={
-                                <Popover key={popoverKey} content={popoverItem} title="Recipe and Amount Needed">
-                                    <div>Needed amount for all recipes: {value.quantity}</div>
-                                </Popover>
-                            }
-                            /> 
-                            <Checkbox id={curId + '-checkbox'} onChange={this.onChange}></Checkbox>
-                        </List.Item>
-                    )
-
-                    curId++;
-                }
-
-                // update the page once the api call is complete
-                this.setState({ listItem: listItem, curId: curId, listInfo: listInfo})
+                this.updateList(response.data.result, false);
             })
     }
 
     render() {
         return(
-            <Card>
+            <Card style={{ width: '50%', margin: 'auto' }}>
                 <PageHeader
                     title="Shopping List"
                     onBack={() => window.history.back()}
                     subTitle={<span>This list is automatically populated with ingredients from your <a href='meal-planner'>Meal Planner</a>!</span>}
                 >
-
                 </PageHeader>
-                {/* <Button onClick={this.printStatus}>Save</Button> */}
-                <br></br>
-                <br></br>
-                <div style={{ margin: 'auto', width: '30%' }}>
+
+                <div style={{ margin: 'auto' }}>
                     <List
                         bordered
                         header={<div style={{fontSize: '2em'}}>For Your Next Grocery Trip</div>}
+                        style={{marginTop: '50px', backgroundColor: 'white'}}
                     >
                         {this.state.listItem}
                     </List>
                     <List
                         bordered
                         header={<div style={{fontSize: '2em'}}>Ticked Items</div>}
-                        style={{marginTop: '50px'}}
+                        style={{marginTop: '50px', backgroundColor: '#fafafa'}}
                     >
                         {this.state.tickedListItem}
                     </List>
