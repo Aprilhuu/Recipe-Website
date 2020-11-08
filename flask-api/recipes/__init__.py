4940df42f19a0ff1909738767421fa30ba43028a
colorzzr <colorzzr@gmail.com>
2020-11-08 11:29:06 -0500

    add new api to randomly fetch the recipe

--- flask-api/recipes/__init__.py
+++ flask-api/recipes/__init__.py
@@ -1,5 +1,5 @@
 from app import module_api
-from .recipe_api import Recipes, Recipe, RecipeQuery
+from .recipe_api import Recipes, Recipe, RecipeQuery, RecipesRadom
 
 
 # create the recipe namespace
@@ -9,3 +9,5 @@ recipe_ns = module_api.namespace(name='recipe_api', path='/v1/recipes')
 recipe_ns.add_resource(Recipes, '/')
 recipe_ns.add_resource(Recipe, '/<rid>')
 recipe_ns.add_resource(RecipeQuery, '/query')
+
+recipe_ns.add_resource(RecipesRadom, '/query/random')
--- flask-api/recipes/recipe_api.py
+++ flask-api/recipes/recipe_api.py
@@ -28,13 +28,15 @@ class Recipes(Resource):
                 recipes.append({
                     'id': str(x['_id']), 
                     'title': x['title'], 
-                    'description': description + ' ...'
+                    'description': description + ' ...',
+                    'image': x.get('mediaURL').get('url', None)
                 })
 
         except Exception as e:
             return {'result': str(e)}, 400
 
         return {'result':recipes}, 200
+
 
 
 # this api instance deal with the specific recipe
@@ -92,3 +94,39 @@ class RecipeQuery(Resource):
             return {'result': str(e)}, 400
 
         return {'result':recipe}, 200
+
+
+# this api instance is make random number of recipe for front page
+class RecipesRadom(Resource):
+    
+    def get(self):
+        '''
+        Retrieve random number the recipe and title for list displaying
+        '''
+        # set the default random size as 3
+        random_number = request.args.get('size', 3)
+
+        # example curl localhost:5000/v1/recipes/
+        try:
+            collection = db_connection["group3_collection"]
+            # for now just return the 8 recipe in total
+            # to keep minimun only return the id and title of list
+            cursor = collection.aggregate([{ '$sample': { 'size': random_number } }])
+            recipes = []
+            for x in cursor:
+                # for displaying pick the first instruction
+                description = x['instructions'][0]['description'] if len(x['instructions']) > 0 else ''
+                # some of the instruction are over length so pick first 20 word if greater
+                description = description[:50] if len(description) > 50 else description
+                
+                recipes.append({
+                    'id': str(x['_id']), 
+                    'title': x['title'], 
+                    'description': description + ' ...',
+                    'image': x.get('mediaURL').get('url', None)
+                })
+
+        except Exception as e:
+            return {'result': str(e)}, 400
+
+        return {'result':recipes}, 200
